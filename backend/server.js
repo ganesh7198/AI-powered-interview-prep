d0d5994fcfc48e141b515ac94d8f13add19c40e9
ganesh7198 <gg7198596@gmail.com>
2026-02-05 20:03:30 +0530

    backend start

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config();
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors({
+	origin: "*",
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`App listening at http://localhost:${PORT}`);
+});
