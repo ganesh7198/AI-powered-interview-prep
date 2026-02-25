f6198f25ef058bbfb40f63a697892db52d62d9d2
ganesh7198 <gg7198596@gmail.com>
2026-02-25 20:44:38 +0530

    cookie fixed

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -11,12 +11,12 @@ const generateTokenAndSetCookie = (userId, res) => {
     { expiresIn: "1d" }
   );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 24 * 60 * 60 * 1000
+});
 
   return token;
 };
